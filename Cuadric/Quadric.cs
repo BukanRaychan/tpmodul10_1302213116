9bb29f29a45d6c52bcf7d2a3f963640cbc8c1742
BukanRaychan <fasyaraihan82@gmail.com>
2023-04-30 21:16:28 +0700

    Add project files.

---
+++ Cuadric/Quadric.cs
@@ -0,0 +1,23 @@
+﻿namespace AljabarLibraries;
+
+public static class Cuadric
+{
+    public static double[] AkarPersamaanKuadrat(double[] persamaan)
+    {
+        return new double[]
+        {
+            (-persamaan[1] + Math.Sqrt(Math.Pow(persamaan[1],2) - 4*persamaan[0]*persamaan[2])) / (2*persamaan[0]),
+            (-persamaan[1] - Math.Sqrt(Math.Pow(persamaan[1],2) - 4*persamaan[0]*persamaan[2])) / (2*persamaan[0])
+        };
+    }
+
+    public static double[] HasilKuadrat(double[] persamaan)
+    {
+        return new double[]
+        {
+            Math.Pow(persamaan[0],2),
+            persamaan[0] * persamaan[1] * 2,
+            Math.Pow(persamaan[1],2)
+        };
+    }
+}
---
+++ Main/Program.cs
@@ -0,0 +1,11 @@
+﻿using AljabarLibraries;
+
+Double[] AkarPersamaanKuadrat = AljabarLibraries.Cuadric.AkarPersamaanKuadrat(new double[] {1,-3,-10});
+Double[] HasilKuadrat = AljabarLibraries.Cuadric.HasilKuadrat(new double[] {2, -3});
+
+Console.WriteLine($"Akar Pesamaan Kuadrat X^2-3x-10 = 0 yaitu :\n" +
+    $"X1 = {AkarPersamaanKuadrat[0]}\n" +
+    $"X2 = {AkarPersamaanKuadrat[1]}\n\n" +
+    $"Hasil Quadrat dari (2x-3)^2 : {HasilKuadrat[0]}x^2{HasilKuadrat[1]}x+{HasilKuadrat[2]}\n" +
+    $"Hasil Dalam bentuk array : {{{string.Join(", ", HasilKuadrat)}}}");
+
